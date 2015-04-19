2a0e8b1ccf61c2a4f6b2170d308c5a8c7d86b3ca
schwar12 <schwar12@purdue.edu>
2015-04-19 14:48:35 -0400

    login/logout switch on the index page, session variables now print out on default template

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', user = session['username'])
+    if 'username' in session: # user is logged in
+        return render_template('index.html', user = session['username'])
+    else:
+        return render_template('index.html')
 
 @app.route('/user/<username>')
 def profile():

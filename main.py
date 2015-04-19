3e56ef586953fba1e7e45ff31d1b2e791365b23e
schwar12 <schwar12@purdue.edu>
2015-04-19 13:13:51 -0400

    removed unnecessary stuff from main, and added index page and template

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-import jinja2
-import os
 
 app = Flask(__name__)
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env=jinja2.Environment(loader=jinja2.FileSystemLoader("templates"),autoescape=True)
 
 @app.route("/")
-def main():
+@app.route("/index")
+def index():
+    return render_template('index.html');
+
+@app.route("/login")
+def login():
     return render_template('login.html');
 
 if __name__ == "__main__":

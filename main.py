237c8d6e4d3c0da8a4a7d6dfe3fd2cb85b8e23d8
schwar12 <schwar12@purdue.edu>
2015-04-19 13:33:13 -0400

    added nav bar and default template

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+fakeuser = {'email':'bob@yahoo.com', 'password':'hunter2'}
+
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html');
+    return render_template('index.html', user=fakeuser);
 
 @app.route("/login")
 def login():
-    return render_template('login.html');
+    return render_template('login.html', title='Login');
 
 if __name__ == "__main__":
     app.run(port = 8080, debug = True)

151b19b201a1608590d36ea132cd07509b55e4fe
schwar12 <schwar12@purdue.edu>
2015-04-19 14:30:28 -0400

    login sorta works

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, escape, request
 
 app = Flask(__name__)
-
-fakeuser = {'email':'bob@yahoo.com', 'password':'hunter2'}
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', user=fakeuser);
+    return render_template('index.html', user = session['username'])
 
-@app.route("/login")
+@app.route('/user/<username>')
+def profile():
+    return
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login');
+    if request.method =='POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login')
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(port = 8080, debug = True)

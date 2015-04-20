c759178141215c66e9b89ef53f1bc63d848d6cd4
schwar12 <schwar12@purdue.edu>
2015-04-19 21:48:19 -0400

    added comments

--- models.py
+++ models.py
@@ -5,11 +5,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+
+#friend = db.Table('friend', 
+#            db.Column('userid', db.Integer, db.ForeignKey('User.id')), 
+#            db.Column('friendid', db.Integer, db.ForeignKey('User.id'))
+#            )
+
+#request = db.Table('request', 
+#            db.Column('userid', db.Integer, db.ForeignKey('User.id')), 
+#            db.Column('requestingid', db.Integer, db.ForeignKey('User.id'))
+#            )
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text)
     email = db.Column(db.Text)
     password = db.Column(db.Text)
+
+
+
+    #friends = db.relationship('User', secondary = friend, primaryjoin=(friend.c.userid == id), secondaryjoin=(friend.c.friendid == id), backref = db.backref('friend', lazy='dynamic'), lazy = 'dynamic')
+
+    #requests = db.relationship('User', secondary = request, primaryjoin=(request.c.userid == id), secondaryjoin=(request.c.requestingid == id), backref = db.backref('request', lazy='dynamic'), lazy = 'dynamic')
+
+
 
     def __init__(self, name, email, password):
 	self.name = name
@@ -24,6 +44,8 @@ class Album(db.Model):
     name = db.Column(db.Text)
     ownerid = db.Column(db.Text)
     visibility = db.Column(db.Text)
+    #users = db.relationship('users', backref = db.backref('albums', lazy = 'dynamic'))
+
 
     def __init__(self, name, ownerid, visibility):
         self.name=name
